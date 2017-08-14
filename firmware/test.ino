89f10e4c0d17e6e0cb085f6f122f52f416a7df1f
Sayanee <sayanee@gmail.com>
2017-08-15 06:58:51 +0800

    add test for wakeup on reset

--- firmware/test.ino
+++ firmware/test.ino
@@ -1,6 +1,7 @@
 // INFO: Flash this firmware to test the board functions
 
 #define LED 2 // GPIO02 on ESP-12 module is linked to on-board LED
+#define WAKE_UP_REASON_DEEP_SLEEP 5
 
 #include <ESP8266WiFi.h>
 
@@ -8,16 +9,24 @@
 const char* ssid = "secret";
 const char* password = "secret";
 
+extern "C" {
+  #include "user_interface.h"
+}
+
 void setup() {
   pinMode(LED, OUTPUT);
-	Serial.begin(115200);
+  Serial.begin(115200);
+
+  if (getResetReason() == WAKE_UP_REASON_DEEP_SLEEP) {
+    testWakeUp();
+  }
 
   Serial.println("[INFO] Starting test in 20 seconds...");
   delay(10000);
 
   testBlink();
   testWiFiConnectivity();
-  testWakeUp();
+  testSleep();
 }
 
 void loop() { }
@@ -49,7 +58,28 @@ void testWiFiConnectivity() {
   Serial.println(WiFi.localIP());
 }
 
-void testWakeUp() {
+void testSleep() {
   Serial.print("[TEST] Sleeping now...");
   ESP.deepSleep(0, WAKE_RF_DEFAULT);
+}
+
+void testWakeUp() {
+  Serial.print("[TEST] Woke up after reset");
+}
+
+int getResetReason() {
+  rst_info* ri = system_get_rst_info();
+  if (ri == NULL) {
+    return -1;
+  }
+
+  // REASON_DEFAULT_RST   = 0,  /* normal startup by power on */
+  // REASON_WDT_RST     = 1,  /* hardware watch dog reset */
+  // REASON_EXCEPTION_RST = 2,  /* exception reset, GPIO status won’t change */
+  // REASON_SOFT_WDT_RST    = 3,  /* software watch dog reset, GPIO status won’t change */
+  // REASON_SOFT_RESTART  = 4,  /* software restart ,system_restart , GPIO status won’t change */
+  // REASON_DEEP_SLEEP_AWAKE  = 5,  /* wake up from deep-sleep */
+  // REASON_EXT_SYS_RST      = 6    /* external system reset */
+
+  return ri->reason;
 }
